9f489c5d213da74147e0f5dc7b4076e90dfc4230
liangjiangli <liangjiangli@bluemoon.com.cn>
2016-11-09 15:57:12 +0800

    test

--- app/src/main/java/com/example/eric/photopicker/Constants.java
+++ app/src/main/java/com/example/eric/photopicker/Constants.java
@@ -4,7 +4,6 @@ package com.example.eric.photopicker;
  * Created by eric on 2016/11/9.
  */
 public class Constants {
-
     public static final String ICON_ADD = "000000"; //图片选择默认URL
     /**
      * 点击添加按钮时的requestcode
